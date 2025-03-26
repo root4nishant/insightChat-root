e077daef6fd758ed40ba6480f4681483ed1b6cef
Nishant4coding <srivastava4nishant@gmail.com>
2025-03-27 02:27:14 +0530

    main rearrange

--- main.py
+++ main.py
@@ -1,25 +1,3 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from routers.chat import chat_router
-from routers.user import user_router
-
-
-app = FastAPI()
-
-# Allow CORS for extension and frontend
-# origins = ["http://localhost:3000", "chrome-extension://*", "*"].
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello from FastAPI on Vercel!"}
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 # Routers
 from fastapi import FastAPI
